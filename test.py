e1625deae3dd50052974f4efaf5262be9f95c556
alimilidurgacharan <84910420+alimilidurgacharan@users.noreply.github.com>
2025-12-04 14:17:38 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from requests import request
 
 app = Flask(__name__)
 
@@ -6,8 +7,19 @@ app = Flask(__name__)
 def home():
     return "Hello, World!"
 
-@app.route('/login/', methods=['POST'])
+@app.route('/login_test/', methods=['POST'])
 def login():
     return "Login endpoint"
 
 
+@app.route('login/', methods = ['Post'])
+def login_user():
+    data = request.get_json()
+
+    username = data.get("username")
+    password = data.get("password")
+
+    if username == "admin" and password == "password":
+        return jsonify({"message": "Login successful"}), 200
+    else:
+        return jsonify({"message": "Invalid credentials"}), 401
